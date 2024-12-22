06870400123628116bcbd4edb6e5b4710ab7cf26
god123oriwke4ofjk <163017856+god123oriwke4ofjk@users.noreply.github.com>
2024-12-22 18:45:30 +0200

    Update xrandr.sh

--- xrandr.sh
+++ xrandr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 xrandr --auto
-xrandr --output DP-1 --primary --pos 0x1080 --rotate normal \
-       --output HDMI-0 --pos 1920x1080 --rotate normal \
-       --output HDMI-1 --pos 960x0 --rotate normal
+xrandr --output DP-1 --primary --mode 1920x1080 --rate 60 --pos 0x1080 --rotate normal \
+       --output HDMI-0 --mode 1920x1080 --rate 75 --pos 1920x1080 --rotate normal \
+       --output HDMI-1 --mode 1920x1080 --rate 75 --pos 960x0 --rotate normal
+
